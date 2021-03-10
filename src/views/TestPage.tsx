fc4d351cc7d4eace0b776517c510e4a6a631edfb
zhen <770226915@qq.com>
2021-03-10 15:40:42 +0800

    perf: 添加调用示例

--- src/views/TestPage.tsx
+++ src/views/TestPage.tsx
@@ -1,10 +1,22 @@
-import { defineComponent } from 'vue';
+import { defHttp } from '@/utils/http';
+import { computed, defineComponent } from 'vue';
 import { useRoute } from 'vue-router';
 
 export default defineComponent({
   name: 'TestPage',
   setup() {
     const route = useRoute();
+
+    // defHttp
+    //   .post({
+    //     url: 'https://record-self.situdata.com/tts/getTemplate',
+    //     data: {
+    //       templateId: 1,
+    //     },
+    //   })
+    //   .then((data) => {
+    //     console.log(data);
+    //   });
     return () => <h1>{route.path}</h1>;
   },
 });
